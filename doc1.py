cd2d8f5d185f83cba6ae198db573a880e5c07479
jhonsmith17177 <zero777jc1@gmail.com>
2025-01-06 20:59:16 -0500

    adding files of the project

---
+++ doc1.py
@@ -0,0 +1,1 @@
+print("pan con camote peruano nam nam")
---
+++ doc2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"pan con pollo"<<endl;
+    cout<<"end of the world"<<endl;
+
+    cout<<"rama principal"<<endl;
+    return 0;
+}
