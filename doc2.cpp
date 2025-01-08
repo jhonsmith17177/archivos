5b5b77cb1a8fd3010efdee80523b635dad52d3e4
jhonsmith17177 <zero777jc1@gmail.com>
2025-01-08 17:33:14 -0500

    Update doc2.cpp

--- doc2.cpp
+++ doc2.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main(){
     cout<<"pan con pollo"<<endl;
-    cout<<"end of the world"<<endl;
+    cout<<"i have a hedache"<<endl;
 
     cout<<"rama principal"<<endl;
     return 0;
